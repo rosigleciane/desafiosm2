e902d0933a246adad4e9ff662d9efb3fe50fda59
rosisantos <rosisantos79@gmail.com>
2022-02-03 01:48:34 -0200

    desafios 1, 2 e 3 finalizados

---
+++ atividade3.js
@@ -0,0 +1,12 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
